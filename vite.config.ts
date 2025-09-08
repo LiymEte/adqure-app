7cde47c6177da580e24c49342f526d34c0881c72
Liym Ete <152188469+LiymEte@users.noreply.github.com>
2025-09-08 18:20:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // Minimal config for GitHub Pages
 export default defineConfig({
-  base: "/adqure-app/", // ⬅ replace with your repo name (e.g., /adqure-app/ or /my-app/)
+  base: "/adqure-app/",
 })
